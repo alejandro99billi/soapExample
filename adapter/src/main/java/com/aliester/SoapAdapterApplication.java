77c57416766205fc412edafe532596432c08acf4
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-07-21 21:53:43 +0200

    firts commit

---
+++ adapter/src/main/java/com/aliester/SoapAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.aliester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapAdapterApplication.class, args);
+	}
+
+}
